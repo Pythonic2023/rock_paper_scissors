3286f7c8e8063d86ba0ac0d27fac8776b9f70c7e
pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2026-03-31 18:22:37 -0400

    Add scripts/game.js

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+// Create function called getComputerChoice
+// This function will use math.random which will return a number between 0 and 1. Use this to return,
+// either rock, paper or scissors. Test the returned value with console.log().
+
+// Write function called getHumanChoice, that takes the humans choice and returns their choice to the console.
+// Get choice using the prompt function.
+
+// Keep track of the humanScore and computerScore by creating global variables and initialize value with 0.
+
+// Create function called playRound which will play a single round, and increment the winners score by one and logs
+// a console message for the winner. It will take two params called playerChoice and computerChoice.
+// Make playerChoice case insensitive.
+
