c4030b06d13d90b355d83c3e433ecc10f1bb1933
Pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-03-31 19:34:08 -0400

    Add pseudo code to game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,29 @@
-// Create function called getComputerChoice
-// This function will use math.random which will return a number between 0 and 1. Use this to return,
-// either rock, paper or scissors. Test the returned value with console.log().
+// humanScore = 0;
+//computerScore = 0;
 
-// Write function called getHumanChoice, that takes the humans choice and returns their choice to the console.
-// Get choice using the prompt function.
+// FUNCTION getComputerChoice
+    // pick rock, paper or scissors using math.random
+    // LOG choice
 
-// Keep track of the humanScore and computerScore by creating global variables and initialize value with 0.
+// FUNCTION getHumanChoice
+    // PROMPT to get players choice, store in var
+    // LOG choice
 
-// Create function called playRound which will play a single round, and increment the winners score by one and logs
-// a console message for the winner. It will take two params called playerChoice and computerChoice.
-// Make playerChoice case insensitive.
+// FUNCTION playRound(PARAM, PARAM)
+    // define which object beats what
+    // declare winner
+    // LOG winner
+    // incremement winners score
 
+
+// AFTER THE ABOVE WORKS
+
+// FUNCTION playGame
+    // score variables moved here
+    // playRound code moved here 
+
+    // WHILE playedRounds less than or equal 5
+        // IF tie
+            // point to each
+        // elif 
+            // Test who wins
