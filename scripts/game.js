cda507761b5ee72665eaee247ec0f6c9dad0e38f
pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-04-25 05:53:30 -0400

    Add buttons to index.html with event listeners.

--- scripts/game.js
+++ scripts/game.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
     let getRandomNumber = Math.random();
     
     if(getRandomNumber <= 0.33) {
-        return "rock";
+        return "Rock";
     } else if (getRandomNumber <= 0.66) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
@@ -16,38 +16,62 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame(computerSelection, humanSelection){
     let keepGoing = true;
     let counter = 0;
-
+    playRound(computerSelection, humanSelection);
     function playRound(computerSelection, humanSelection) {
-        if(computerSelection === "rock" && humanSelection === "scissors"){
+        if(computerSelection === "Rock" && humanSelection === "Scissors"){
             computerScore += 1;
-        } else if(computerSelection === "scissors" && humanSelection === "paper"){
-            computerScore += 1;
-        } else if(computerSelection === "paper" && humanSelection === "rock"){
-            computerScore += 1;
+            displayComputer.textContent = `Computer Score: ${computerScore}`;
+        } else if(computerSelection === "Scissors" && humanSelection === "Paper"){
+            displayComputer.textContent = `Computer Score: ${computerScore}`;
+        } else if(computerSelection === "Paper" && humanSelection === "Rock"){
+            displayComputer.textContent = `Computer Score: ${computerScore}`;
         } else {
             humanScore += 1;
+            displayHuman.textContent = `Human Score: ${humanScore}`;
             }
         }
-    
     while(keepGoing){
         let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
+        let humanChoice = humanSelection;
         playRound(computerSelection, humanSelection);
         counter += 1
         if(counter === 5){
             keepGoing = false;
             if(computerScore > humanScore){
-                console.log(`Computer won with a score of ${computerScore} out of 5`);
+                endGameScore.textContent = `Computer won with a score of ${computerScore} out of 5`
             } else {
-                console.log(`Human won with a score of ${humanScore} out of 5`);
+                endGameScore.textContent = `Human won with a score of ${humanScore} out of 5`;
             }
         }
-    }
+    } 
 }
 
-playGame();
+let rpsDiv = document.querySelector('.rps-buttons');
+const displayComputer = document.querySelector('.computer-score');
+const displayHuman = document.querySelector('.human-score');
+const endGameScore = document.querySelector('.end-game-score');
+
+const rock = document.createElement('button');
+rock.innerText = "Rock";
+rpsDiv.appendChild(rock);
+
+const paper = document.createElement('button');
+paper.innerText = "Paper";
+rpsDiv.appendChild(paper);
+
+const scissors = document.createElement('button');
+scissors.innerText = "Scissors";
+rpsDiv.appendChild(scissors);
+
+rpsDiv.addEventListener('click', (event) => {
+    let computerChoice = getComputerChoice();
+    let playerChoice = event.target.innerText;
+    console.log(playerChoice);
+    playGame(computerChoice, playerChoice);
+})
